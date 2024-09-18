cc60731b900ed8672a2f2ec43c980c2474a50250
fazalpannu <fazalpannu22@gmail.com>
2024-09-18 17:20:13 +0500

    Initial Commit

---
+++ userlist/userlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  userlistApp.swift
+//  userlist
+//
+//  Created by Ml Bench on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct userlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
