17d55edcc6b43faa3c6d054ccf796d5206733399
fazalpannu <fazalpannu22@gmail.com>
2024-09-23 10:00:30 +0500

    anew

---
+++ userlist/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  userlist
+//
+//  Created by Ml Bench on 19/09/2024.
+//
+
+import Foundation
---
+++ userlist/model.swift
@@ -0,0 +1,8 @@
+//
+//  model.swift
+//  userlist
+//
+//  Created by Ml Bench on 19/09/2024.
+//
+
+import Foundation
